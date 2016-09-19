dfd1a4ed1143e73bfb53be280fde2f12b2aaf2fe
Josef Vlach <vlach.josef@gmail.com>
2016-09-19 21:21:53 +0100

    Step 2

--- src/main/scala/com/foldright/CheckoutSystem.scala
+++ src/main/scala/com/foldright/CheckoutSystem.scala
@@ -3,31 +3,41 @@ package com.foldright
 package checkout
 
 import cats.{ Monoid, Show }
+import cats.implicits._
 
-case class Checked(sum: BigDecimal)
-
-object Checked {
-  implicit val m = new Monoid[Checked] {
-    def combine(c1: Checked, c2: Checked): Checked = Checked(c1.sum + c2.sum)
-    def empty: Checked = Checked(0)
-  }
-}
-
-sealed trait Fruit
-case object Apple extends Fruit
-case object Orange extends Fruit
-
-object CheckoutSystem {
-
-  def price(f: Fruit): BigDecimal = f match {
+sealed trait Fruit {
+  def price: BigDecimal = this match {
     case Apple => 60
     case Orange => 25
   }
+}
+case object Apple extends Fruit
+case object Orange extends Fruit
 
-  def toChecked(f: Fruit): Checked = Checked(price(f))
+case class Checked(sum: BigDecimal, map: Map[Fruit, Int]) {
+  private def calculatePrice(f: Fruit, buy: Int, free: Int): BigDecimal = {
+    val size = map.getOrElse(f, 0)
+    ((size % buy) + ((size / buy) * free)) * f.price
+  }
+
+  def totalSum: BigDecimal = {
+    List((Apple, 2, 1), (Orange, 3, 2)).map { case (f, buy, free) => calculatePrice(f, buy, free) }.sum
+  }
+}
+
+object Checked {
+  implicit val m = new Monoid[Checked] {
+    def combine(c1: Checked, c2: Checked): Checked = Checked(c1.sum + c2.sum, c1.map.combine(c2.map))
+    def empty: Checked = Checked(0, Map.empty[Fruit, Int])
+  }
+}
+
+object CheckoutSystem {
+
+  def toChecked(f: Fruit): Checked = Checked(f.price, Map(f -> 1))
 
   def checkout(fruits: List[Fruit]): BigDecimal = {
-    Monoid[Checked].combineAll(fruits.map(toChecked)).sum
+    Monoid[Checked].combineAll(fruits.map(toChecked)).totalSum
   }
 }
 
--- src/test/scala/com/foldright/CheckoutSystemSuite.scala
+++ src/test/scala/com/foldright/CheckoutSystemSuite.scala
@@ -22,9 +22,45 @@ class CheckoutSystemSuite extends FlatSpec with Matchers {
   import convertors._
   import cats.syntax.show._
 
-  it should "checkout List of Fruits and print result in pounds" in {
-    val fruits = List(Apple, Apple, Orange, Apple)
-    val res = CheckoutSystem.checkout(fruits).show
-    res should be("£2.05")
+  it should "checkout 1 and 2 apples for the price of 1 apple" in {
+    val apple1 = List(Apple)
+    val apple2 = List(Apple, Apple)
+    val apple1Res = CheckoutSystem.checkout(apple1).show
+    val apple2Res = CheckoutSystem.checkout(apple2).show
+    apple1Res should be(apple2Res)
+    apple1Res should be("£0.6")
+  }
+
+  it should "checkout 3 and 4 apples for the price of 2 apples" in {
+    val apple3 = List(Apple, Apple, Apple)
+    val apple4 = List(Apple, Apple, Apple, Apple)
+    val apple3Res = CheckoutSystem.checkout(apple3).show
+    val apple4Res = CheckoutSystem.checkout(apple4).show
+    apple3Res should be(apple4Res)
+    apple3Res should be("£1.2")
+  }
+
+  it should "checkout 2 and 3 oranges for the price of 2 oranges" in {
+    val oranges2 = List(Orange, Orange)
+    val oranges3 = List(Orange, Orange, Orange)
+    val oranges2Res = CheckoutSystem.checkout(oranges2).show
+    val oranges3Res = CheckoutSystem.checkout(oranges3).show
+    oranges2Res should be(oranges3Res)
+    oranges2Res should be("£0.5")
+  }
+
+  it should "checkout 5 and 6 oranges for the price of 4 oranges" in {
+    val oranges5 = List(Orange, Orange, Orange, Orange, Orange)
+    val oranges6 = List(Orange, Orange, Orange, Orange, Orange, Orange)
+    val oranges5Res = CheckoutSystem.checkout(oranges5).show
+    val oranges6Res = CheckoutSystem.checkout(oranges6).show
+    oranges5Res should be(oranges6Res)
+    oranges5Res should be("£1")
+  }
+
+  it should "checkout 6 oranges and 4 apples for the price of 4 oranges and 2 apples" in {
+    val fruits = List(Orange, Orange, Orange, Orange, Orange, Orange) ++ List(Apple, Apple, Apple, Apple)
+    val fruitsRes = CheckoutSystem.checkout(fruits).show
+    fruitsRes should be("£2.2")
   }
 }
